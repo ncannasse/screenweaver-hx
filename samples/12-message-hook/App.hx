cd354b50cd23062c2878f0220696ae61f21a52fb
edwin.vanrijkom <edwin.vanrijkom@cdf03ea7-1d22-0410-9e40-df6fa58ffaa3>
2007-03-09 13:58:44 +0000

    sample 12 - removed tray/menu.

--- samples/12-message-hook/App.hx
+++ samples/12-message-hook/App.hx
@@ -1,5 +1,3 @@
-import systools.win.Tray;
-import systools.win.Menus;
 import systools.win.Events;
 
 class App {
@@ -8,9 +6,6 @@ class App {
 	static var cnx: swhx.Connection;
 	static var window: swhx.Window;
 	static var hook: swhx.MessageHook;
-	static var thook: swhx.MessageHook;
-	static var tray: Tray;
-	static var m: Menus;
 
     static function main() {
     	 // initialize ScreenWeaver HX
@@ -53,16 +48,9 @@ class App {
     	cnx = swhx.Connection.flashConnect(flash);
     	window.show(true);
     	trace("Window handle is:"+window.handle+" (will be readable when passed to an .ndll)");
-		
-		m = new systools.win.Menus();
-		m.addItem( "option 1", 1 );
-		m.addItem( "option 2", 2 );
-		m.addItem( "option 3", 3 );
-		
+				
     	hook = window.addMessageHook(untyped Events.RBUTTONUP /*WM_RBUTTONUP on Windows*/);
-		hook.setNekoCallback(mouseRightClickHook);
-    	thook = window.addMessageHook(untyped Events.TRAYEVENT);
-		thook.setNekoCallback(mouseRightClickHook);
+		hook.setNekoCallback(mouseRightClickHook);    	
     	/*
     	
     	Additionally, C to C hooks can be set. When set, the C handler takes
@@ -77,15 +65,11 @@ class App {
     	hook.setCCallback(|function-ptr-abstract-value|);
 		*/		
     }
-    
-    static function mouseMoveHook() {    	
-		trace("Windows Mouse Move!" +"("+hook.p1+","+hook.p2+","+hook.callbackData+")");
-    	return 0;
-    }    
-	
+    	
 	static function mouseRightClickHook() {
-		if ( Std.string(thook.p2) == Std.string(Events.RBUTTONUP) )
-			trace( "Option " + m.show( window.handle ) + " was selected!" );
+		trace( "Right mouse click detected! (P1:"+hook.p1+", P2: "+hook.p2+")" );
+		// return message 'not-handled', so SWHX will continue
+		// processing it:
 		return 0;
 	}
 }
