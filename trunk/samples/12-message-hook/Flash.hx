71155cc4b3682f93d99fce4eb41f181af5772b2e
edwin.vanrijkom <edwin.vanrijkom@cdf03ea7-1d22-0410-9e40-df6fa58ffaa3>
2007-03-09 13:58:44 +0000

    sample 12 - removed tray/menu.

--- trunk/samples/12-message-hook/Flash.hx
+++ trunk/samples/12-message-hook/Flash.hx
@@ -4,6 +4,10 @@ class Flash {
     static var enabledCom = swhx.Connection;
     
     static function main() {
-		trace("Run this sample from console to see window messages being printed.");	
+		trace(
+			"Run this sample from console to see\n"+
+			"window messages being printed,\n"+
+			"on Windows systems, on Right Click."
+		);	
     }   
 }
