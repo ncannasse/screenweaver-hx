a2da218a6b4f0e1d5572651dd6ffdd4ca179924e
ncannasse <ncannasse@cdf03ea7-1d22-0410-9e40-df6fa58ffaa3>
2008-09-11 16:09:22 +0000

    minor fix

--- samples/sampler/backend/Sample.hx
+++ samples/sampler/backend/Sample.hx
@@ -7,9 +7,9 @@ class Sample {
 	public var buildCommands(getBuildCmds,null): Array<String>;
 	public var runCommands(getRunCmds,null): Array<String>;
 	public var build(default,default): Bool;
-	
+
 	private var xml: Xml;
-	private var parsed: Bool;	
+	private var parsed: Bool;
 
 	public function new(path: String, file: String) {
 		this.path = path;
@@ -17,29 +17,29 @@ class Sample {
 		this.parsed = false;
 		this.build = false;
 	}
-	
-	public function parse() : Bool {		
-		parsed = false;				
-		var content = neko.io.File.getContent(file);		
+
+	public function parse() : Bool {
+		parsed = false;
+		var content = neko.io.File.getContent(file);
 		try {
 			xml = Xml.parse(content);
 			parsed = true;
 		}
-		catch(e: Dynamic) {		
-		}		
+		catch(e: Dynamic) {
+		}
 		return parsed;
 	}
-	
-	public function getTitle() : String {		
-		try {			
+
+	public function getTitle() : String {
+		try {
 			return xml.firstElement().get("title");
-		} 
+		}
 		catch (e: Dynamic) {
 			return "Invalid XML: "+e;
 		}
 	}
-	
-	public function getDescription() : String {		
+
+	public function getDescription() : String {
 		try {
 			var xmlfile = xml.firstElement().get("description");
 			return neko.io.File.getContent(path+"/"+xmlfile);
@@ -48,34 +48,34 @@ class Sample {
 			return "Invalid XML: "+e;
 		}
 	}
-	
-	public function getBuildCmds(): Array<String> {		
+
+	public function getBuildCmds(): Array<String> {
 		var node = xml.firstElement().elementsNamed("Build");
 		return getCommands(node);
 	}
-	
-	public function getRunCmds(): Array<String> {		
+
+	public function getRunCmds(): Array<String> {
 		var node = xml.firstElement().elementsNamed("Run");
 		return getCommands(node);
 	}
-	
+
 	static public function getCommands(node: Iterator<Xml>): Array<String> {
 		var result = new Array();
 		for (n in node) {
 			var elements = n.elements();
-			for(e in elements) {				
+			for(e in elements) {
 				var cmd;
 				// select command:
-				if (e.nodeName=="Haxe") {					
+				if (e.nodeName=="Haxe") {
 					cmd = Sampler.p.cmdHaxe;
-				} else if (e.nodeName=="MTASC") {					
-					cmd = Sampler.p.cmdMTASC;									
+				} else if (e.nodeName=="MTASC") {
+					cmd = Sampler.p.cmdMTASC;
 				} else if (e.nodeName=="Manual") {
 					// cannot be build or run:
 					return null;
-				} else if (e.nodeName=="Neko") {					
-					cmd = Sampler.p.cmdNeko+cmd;
-				} else if (e.nodeName=="Boot") {					
+				} else if (e.nodeName=="Neko") {
+					cmd = Sampler.p.cmdNeko;
+				} else if (e.nodeName=="Boot") {
 					cmd = Sampler.p.cmdBoot;
 				} else {
 					cmd = e.nodeName;
@@ -86,7 +86,7 @@ class Sample {
 				// store command:
 				result.push(cmd);
 			}
-		}	
+		}
 		return result;
-	}	
+	}
 }
